389a720534e428df29409e4b10287c4e776ff184
Sammuel <sammuel.pimentel08@gmail.com>
2025-11-14 15:59:35 -0300

    Update UsuarioController to use JWT username for auditing actions

--- src/main/java/br/com/fabreum/AppProdutos/controller/UsuarioController.java
+++ src/main/java/br/com/fabreum/AppProdutos/controller/UsuarioController.java
@@ -2,8 +2,10 @@ package br.com.fabreum.AppProdutos.controller;
 
 import br.com.fabreum.AppProdutos.entity.Usuario;
 import br.com.fabreum.AppProdutos.service.UsuarioService;
+import br.com.fabreum.AppProdutos.service.AuditoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,16 +15,23 @@ import java.util.List;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+    private final AuditoriaService auditoriaService;
 
     @Autowired
-    public UsuarioController(UsuarioService usuarioService) {
+    public UsuarioController(UsuarioService usuarioService, AuditoriaService auditoriaService) {
         this.usuarioService = usuarioService;
+        this.auditoriaService = auditoriaService;
     }
 
     // Endpoint para listar todos os usuários
     @GetMapping
     public ResponseEntity<List<Usuario>> listarUsuarios() {
         List<Usuario> usuarios = usuarioService.listarUsuarios();
+
+        // Registrar auditoria com username do JWT
+        String username = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        auditoriaService.registrar(username, "LIST_USERS", "Listed all users");
+
         return ResponseEntity.ok(usuarios);
     }
 
@@ -30,6 +39,11 @@ public class UsuarioController {
     @PostMapping
     public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario) {
         Usuario salvo = usuarioService.salvarUsuario(usuario);
+
+        // Registrar auditoria com username do JWT
+        String username = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        auditoriaService.registrar(username, "CREATE_USER", "Created user: " + salvo.getUsername());
+
         return ResponseEntity.ok(salvo);
     }
 }
